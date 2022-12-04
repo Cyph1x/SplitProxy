f5f198ef949f1fa769b43197d14e60ccb1eacb7b
joshua ward <email@gmail.com>
2022-12-04 18:17:38 +1000

    updating files

--- start.bat
+++
@@ -1,3 +0,0 @@
-start /d ".\connection split server\bin\Debug\net5.0\" "connection split server.exe"
-start /d ".\connection split client\bin\Debug\net5.0\" "connection split client.exe"
-start /d ".\proxy server\bin\Debug\net5.0\" "proxy server.exe"
